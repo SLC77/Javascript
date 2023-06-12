2947450ffd8baf72b1e254c7777b2649fec5485c
Thibaut <tbusschaertlille@gmail.com>
2023-06-12 15:23:20 +0200

    Docs: JS classe 120623

--- JS class.js
+++ JS class.js
@@ -1,3 +1,17 @@
-let numberOfCats = 2;
-let numberOfDogs = 6;
-var world = 2
+let value = true;
+value = `Thibaut`
+alert(typeof value);
+
+value = String(value);
+alert(typeof value);
+alert(value);
+
+
+
+let str = `123`
+
+alert(typeof str);
+
+let num = Number(str);
+alert(typeof num);
+alert(str)
