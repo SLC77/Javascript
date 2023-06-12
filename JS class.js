17ff89ffa0c60a69541e680be72acb0eefd454fe
Thibaut <tbusschaertlille@gmail.com>
2023-06-12 16:57:31 +0200

    Docs: JS classe 120623

--- JS class.js
+++ JS class.js
@@ -1,17 +1,1 @@
-let value = true;
-value = `Thibaut`
-alert(typeof value);
-
-value = String(value);
-alert(typeof value);
-alert(value);
-
-
-
-let str = `123`
-
-alert(typeof str);
-
-let num = Number(str);
-alert(typeof num);
-alert(str)
+alert(1 && null && 2);
